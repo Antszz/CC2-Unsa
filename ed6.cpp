974cfa8d3c7e2e08d9741f56f02b209642784629
Antszz <38092020+Antszz@users.noreply.github.com>
2018-04-09 00:47:13 -0500

    Update ed6.cpp

--- ed6.cpp
+++ ed6.cpp
@@ -11,7 +11,15 @@ int lenP(char *c){
 	return c-a;
 }
 
-void rempla(char *a,char *b){
+int len( char c[]){
+    int con=0;
+	while(c[con]!='\0'){
+		con++;
+	}
+	return con;
+}
+
+void remplaP(char *a,char *b){
 	char *d=a;
 	while(*b!='\0'){
 		*a = *b;
@@ -21,15 +29,29 @@ void rempla(char *a,char *b){
 	a=d;
 }
 
+void rempla(char x[], char y[]){
+	int t=len(y);
+	for(int i=0;i<t;i++){
+		x[i]=y[i];
+	}
+}
+
 int main(){
 	char *a=new char();
 	char *b=new char();
 	cin>>a;
 	cin>>b;
-	rempla(a,b);
+	remplaP(a,b);
 	for(int i=0;i<lenP(b);i++){
 		cout<<*a;
 		a++;
+	}
+	cout<<endl;
+	char x[]={"mono"};
+	char y[]={"plaza"};
+	rempla(x,y);
+	for(int i=0;i<len(y);i++){
+		cout<<x[i];
 	}
 	cout<<endl;
 	return 0;
