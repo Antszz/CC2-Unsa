be8bcb00776111cd98360eb7931555df6ea3c0f9
Antszz <38092020+Antszz@users.noreply.github.com>
2018-04-09 00:20:42 -0500

    Update ed7.cpp

--- ed7.cpp
+++ ed7.cpp
@@ -1,83 +1,40 @@
-/*#include <iostream>
-#include <string.h>
-using namespace std;
-
-int tam(string c){
-	int con=0;
-	while(c[con]!='\0'){
-		con++;
-	}
-	return con;
-}
-
-char *concatenar(char *a,char *b){
-	int x=tam(a);
-	int y=tam(b);
-	int c=x+y;
-	char *d=new char(c);
-	for(int i=0;i<x;i++){
-		*(d+i)=*(a+i);
-	}
-	for(int j=x;j<c;j++){
-		*(d+j)=*(b+j-x);
-		cout<<*(b+j-x)<<endl;
-	}
-	return d;
-}
-
-int main(){
-	char *a=new char();
-	char *b=new char();
-	cin>>a;
-	cin>>b;
-	cout<<concatenar(a,b)<<endl;
-	return 0;
-}*/
-
 #include <iostream>
+
 using namespace std;
 
-int tam(char *c){
-	int con=0;
-	while(*(c+con)!='\0'){
-		con++;
+int lenP(char *c){
+	char *a;
+	a=c;
+	while(*c!='\0'){
+		c++;
 	}
-	return con;
+	return c-a;
 
 }
-int tamarr( char c[]){
+int len( char c[]){
     int con=0;
 	while(c[con]!='\0'){
 		con++;
 	}
 	return con;
-
-
-
-
 }
 
-
-
-char *concatenar(char *a,char *b){
-	int x=tam(a);
-	int y=tam(b);
-	int c=x+y;
-	char *d=new char(c);
-	for(int i=0;i<x;i++){
-		*(d+i)=*(a+i);
+void *concatenar(char *a,char *b){
+	int x=lenP(a);
+	int y=lenP(b);
+	char *d=a;
+	a=a+lenP(a);
+	while(*b!='\0'){
+		*a = *b;
+		a++;
+		b++;
 	}
-	for(int j=x;j<c;j++){
-		*(d+j)=*(b+(j-x));
-	}
-
-	return d;
+	a=d;
 }
 
 void conca(char a[],char b[]){
-    int tam1=tamarr(a);
-    int tam2=tamarr(b);
-    cout<<tam1<<" "<<tam2<<endl;
+    int tam1=len(a);
+    int tam2=len(b);
     int tamtot=tam1+tam2;
     char nueva[tamtot];
     for(int i=0;i<tam1;i++){
@@ -85,17 +42,12 @@ void conca(char a[],char b[]){
     }
     for(int j=0;j<tam2;j++){
         nueva[j+tam1]=b[j];
-
-
     }
-    //cout<<nueva<<endl;
     for (int i = 0; i < tamtot; i++)
     {
     	cout<<nueva[i];
     }
     cout<<endl;
-
-
 }
 
 
@@ -108,9 +60,11 @@ int main(){
 	char one[]={"HOLA"};
 	char two[]={"AMIGO"};
 	conca(one,two);
-
-	cout<<concatenar(a,b)<<endl;
+	concatenar(a,b);
+	while(*a!='\0'){
+		cout<<*a;
+		a++;
+	}
+	cout<<endl;
 	return 0;
-
-
 }
